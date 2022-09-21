148c319816c07bdda0c5ff690a4de7dc1790b033
Grgur Dujmovic <grga281@gmail.com>
2022-09-21 13:54:02 +0200

    Added 2 constructors for satellite

---
+++ satellite.py
@@ -0,0 +1,17 @@
+class Satellite:
+    def __init__(self, a, e, i, omega, w, theta):
+        self.a = a
+        self.e = e
+        self.i = i
+        self.omega = omega
+        self.w = w
+        self.theta = theta
+    
+    def __init__(self, x, y, z, dx, dy, dz):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.dx = dx
+        self.dy = dy
+        self.dz = dz
+        
